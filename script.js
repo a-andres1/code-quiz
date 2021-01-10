262bbfcf765570e3248371911370cf9d3211ab87
xena <74474948+a-andres1@users.noreply.github.com>
2021-01-10 18:30:35 -0500

    put both my functions under 1 event listener

--- script.js
+++ script.js
@@ -34,11 +34,11 @@ function countdown() {
 
 
 // Welcome Page
-// Variables from Welcome page
+// Variables from Welcome message
 var welcomeBanner = document.getElementById("welcomemessage");
 var startBtn = document.getElementById("startbtn");
 
-// creates divs for Welcome page
+// creates divs for Welcome message
 var header = document.createElement("h1");
 var message = document.createElement("p");
 
@@ -53,25 +53,29 @@ welcomeBanner.appendChild(message);
 welcomeBanner.appendChild(startBtn);
 
 // add event listeners for Start button
-startBtn.addEventListener("click", displayCard);
-startBtn.addEventListener("click", countdown);
+
+startBtn.addEventListener("click", function() {
+  displayCard();
+  countdown();
+})
 
 
 // Quiz Logic
 
 // variables for quiz card
 var card = document.getElementById("card")
-var question = document.getElementById("question");
 var ans1 = document.getElementById("answer1");
 var ans2 = document.getElementById("answer2");
 var ans3 = document.getElementById("answer3");
 var ans4 = document.getElementById("answer4");
+var question = document.getElementById("question");
 
 //  initially hides card
 card.style.display = "none";
 
 // fuction to display the Quiz card on click
 function displayCard() {
+  console.log("Displaycard function ran")
   // once the start button is clicked, sets the card to its initial state
   if (card.style.display === "none") {
     card.style.display = "initial";
