545d1d880b3c35b147b35c06f384e4b4ecd8c40b
xena <74474948+a-andres1@users.noreply.github.com>
2021-01-10 18:06:33 -0500

    created fucntion to hide/display quiz card

--- script.js
+++ script.js
@@ -1,70 +1,84 @@
 
 // Countdown timer
 function countdown() {
-    var timeLeft = 240
-    var timeLeftEl = document.getElementById("timeLeft");
+  var timeLeft = 240
+  var timeLeftEl = document.getElementById("timeLeft");
 
-    timeLeftEl.innerHTML = timeLeft
-    
-    console.log(timeLeftEl)
+  timeLeftEl.innerHTML = timeLeft
+
+  console.log(timeLeftEl)
+  console.log(timeLeft)
+
+  var timerInterval = setInterval(function () {
+
     console.log(timeLeft)
 
-    var timerInterval = setInterval(function() {
+    if (timeLeft === 1) {
+      timeLeftEl.innerHTML = (timeLeft--) + " second left";
 
-        console.log(timeLeft)
-  
-      if (timeLeft === 1) {
-        timeLeftEl.innerHTML = (timeLeft--) + " second left";
+    }
 
-      }
-      
-      else if (timeLeft > 0) {
-          timeLeftEl.innerHTML = (timeLeft--) + " seconds left";
-      }
+    else if (timeLeft > 0) {
+      timeLeftEl.innerHTML = (timeLeft--) + " seconds left";
+    }
 
-      else {
-        clearInterval(timerInterval);
-        alert("Time's up.")
-      }
-  
-    }, 1000);
+    else {
+      clearInterval(timerInterval);
+      alert("Time's up.")
+    }
+
+  }, 1000);
+}
+
+// countdown();
+
+
+// Welcome Page
+// Variables from Welcome page
+var welcomeBanner = document.getElementById("welcomemessage");
+var startBtn = document.getElementById("startbtn");
+
+// creates divs for Welcome page
+var header = document.createElement("h1");
+var message = document.createElement("p");
+
+// styling for the Welcome message div
+document.getElementById("welcomemessage").style.textAlign = "center";
+
+// creates content 
+header.textContent = "Coding Quiz";
+message.textContent = "Try to answer the following javasript questions within the given time. Any incorrect answers will take 5 seconds off your remaining time."
+welcomeBanner.appendChild(header);
+welcomeBanner.appendChild(message);
+welcomeBanner.appendChild(startBtn);
+
+// add event listeners for Start button
+startBtn.addEventListener("click", displayCard);
+startBtn.addEventListener("click", countdown);
+
+
+// Quiz Logic
+
+// variables for quiz card
+var card = document.getElementById("card")
+var question = document.getElementById("question");
+var ans1 = document.getElementById("answer1");
+var ans2 = document.getElementById("answer2");
+var ans3 = document.getElementById("answer3");
+var ans4 = document.getElementById("answer4");
+
+//  initially hides card
+card.style.display = "none";
+
+// fuction to display the Quiz card on click
+function displayCard() {
+  // once the start button is clicked, sets the card to its initial state
+  if (card.style.display === "none") {
+    card.style.display = "initial";
   }
-
-  // countdown();
-
-
-  // Welcome Page
-  var welcomeBanner = document.getElementById("welcomemessage");
-  var startBtn = document.getElementById("startbtn");
-  var header = document.createElement("h1");
-  var message = document.createElement("p");
-  
-
-  document.getElementById("welcomemessage").style.textAlign = "center";
-
-
-  header.textContent = "Coding Quiz";
-  message.textContent = "Try to answer the following javasript questions within the given time. Any incorrect answers will take 5 seconds off your remaining time."
-  welcomeBanner.appendChild(header);
-  welcomeBanner.appendChild(message);
-  welcomeBanner.appendChild(startBtn);
-
-  // add event listener to Start button
-  startBtn.addEventListener("click", countdown);
-
-
-
-
-  // Quiz
-  var card = document.getElementById("card")
-  var question = document.getElementById("question");
-  var ans1 = document.getElementById("answer1");
-  var ans2 = document.getElementById("answer2");
-  var ans3 = document.getElementById("answer3");
-  var ans4 = document.getElementById("answer4");
-
-  function hideCard() {
-    document.getElementById("card").style.display = "none";
+// if start button is clicked again, will rehide the card
+  else {
+    card.style.display = "none"
   }
+}
 
-  hideCard();
